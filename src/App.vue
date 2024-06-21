29e4cb6f386c6d1c64142ee5fa63408b7d5341e8
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-21 22:08:04 +0530

    created a form that can added todo's

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,79 @@
 <script setup>
-import { ref, onMounted , computed , watch } from 'vue'
+import { ref, onMounted, computed, watch } from "vue";
 
-const todos=ref([])
-const name = ref('')
+const todos = ref([]);
+const name = ref("");
 
-const input_content=ref('')
-const input_category=ref(null)
+const input_content = ref("");
+const input_category = ref(null);
 
-const todos_asc = computed(()=> todos.value.sort((a,b)=>{
-    return b.createdAt - a.createdAt
-}))
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
 
-watch(name,(newVal)=>{
-    localStorage.setItem('name', newVal)
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
 
-})
-
-onMounted(()=>{
-    name.value=localStorage.getItem('name') || ''
-})
-
-  
-
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
 
 <template>
-    <main class="app">
-        <section class="greeting">
-          <h2 class="title">What's up,
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up,
 
-             <input type="text" placeholder="Name Here" v-model="name"/>
-          </h2>
-         
-        </section>
-    </main>
- 
+        <input type="text" placeholder="Name Here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form id="new-todo-form" @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+  </main>
 </template>
-
