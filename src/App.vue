cf27d96801abaee913d5db6728bbf7a7d4fcf681
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-21 12:08:45 +0530

    created an input to set the name and mounted to get the value from the local storage each time page refreshes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script setup>
+import { ref, onMounted , computed , watch } from 'vue'
+
+const todos=ref([])
+const name = ref('')
+
+const input_content=ref('')
+const input_category=ref(null)
+
+const todos_asc = computed(()=> todos.value.sort((a,b)=>{
+    return b.createdAt - a.createdAt
+}))
+
+watch(name,(newVal)=>{
+    localStorage.setItem('name', newVal)
+
+})
+
+onMounted(()=>{
+    name.value=localStorage.getItem('name') || ''
+})
+
+  
 
 </script>
 
 <template>
-<div>
-  Hello World
-</div>
+    <main class="app">
+        <section class="greeting">
+          <h2 class="title">What's up,
+
+             <input type="text" placeholder="Name Here" v-model="name"/>
+          </h2>
+         
+        </section>
+    </main>
  
 </template>
 
